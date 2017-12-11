3bf6f284973606e0482016856ef174c28cfb5fc5
hyp <yipenghuang@hotmail.com>
2017-12-11 21:09:19 +0800

    index.js

---
+++ quzsc-web-base/index.js
@@ -0,0 +1,8 @@
+
+
+WebBase = {};
+WebBase.seq_exe_func = require("lib/seq_exe_func");
+WebBase.mysql_service = require("lib/mysql_service");
+WebBase.sc_logger = require("lib/sc_logger");
+
+module.exports=WebBase;
